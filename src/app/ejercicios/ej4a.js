8d8babed174e0883cf7e5bdb229cac87a15b0bd4
a23aaronar <a23aaronar@iesteis.es>
2025-01-15 12:40:50 +0100

    prueba para ver si funciona

--- src/app/ejercicios/ej4a.js
+++ src/app/ejercicios/ej4a.js
@@ -2,6 +2,14 @@ import numAleatorio from "./helpers/numRandom";
 
 export default function Ej4A() {
     return (
-        <div></div>
+        <div>
+            <button type="button" onClick={numAleatorio(0,9)}>
+                Cambio el número!
+            </button>
+
+            <h2>
+                {numAleatorio(0, 9)}
+            </h2>
+        </div>
     );
 }
